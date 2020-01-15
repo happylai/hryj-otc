537e9c4b201047b1337dc3b30ae451f3b153a5c7
louis <louislai@qq.com>
2020-01-15 14:09:35 +0800

    角色申请详情 照片放大

--- src/views/user/role/roleDetail.vue
+++ src/views/user/role/roleDetail.vue
@@ -99,7 +99,7 @@
         v-loading="loading"
         align="center"
         label="订单ID"
-        width="65"
+        min-width="120"
         element-loading-text="请给我点时间！"
       >
         <template slot-scope="scope">
@@ -178,10 +178,10 @@
         <el-col :span="8" class="textAlingR">身份证号：</el-col>
         <el-col :span="8">{{ editData.idNumber }}</el-col>
         <el-col :span="4">
-          <img v-if="editData.identityImageFront" v-lazy="editData.identityImageFront" class="idImage" alt="身份证正面">
+          <img v-if="editData.identityImageFront" v-lazy="editData.identityImageFront" @click="dialogVisible=false" :preview="id+'Idcard'" class="idImage" alt="身份证正面">
         </el-col>
         <el-col :span="4">
-          <img v-if="editData.identityImageBack" v-lazy="editData.identityImageBack" class="idImage" alt="身份证反面">
+          <img v-if="editData.identityImageBack" v-lazy="editData.identityImageBack" @click="dialogVisible=false" :preview="id+'Idcard'" class="idImage" alt="身份证反面">
         </el-col>
       </el-row>
       <el-row :gutter="20" class="userRow">
@@ -329,6 +329,7 @@ export default {
       role_apply_detail({ applyId: id || this.id }).then(res => {
         if (res.code === 0) {
           this.modals = res.data
+          this.$previewRefresh()
         }
         console.log('res')
       })

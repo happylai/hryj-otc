f43b3e0b0a442bdf3f109fb9837c11f8a2f0e1c2
louis <louislai@qq.com>
2020-03-26 11:00:39 +0800

    本地配置

--- vue.config.js
+++ vue.config.js
@@ -41,8 +41,8 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://127.0.0.1:${port}/mock`,
-        target: `http://47.57.25.67:4009/api`,
-        // target: `http://192.168.3.185:4009/api`,
+        // target: `http://47.57.25.67:4009/api`,
+        target: `http://192.168.3.144:4009/api`,
         // target: `http://192.168.3.185:4009/api`,
 
         changeOrigin: true,
